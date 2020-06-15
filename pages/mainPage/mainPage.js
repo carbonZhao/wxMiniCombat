244de5480f8a5f34a3208922e1ca2009ad43f3f8
zhaoxiaopeng <123456789>
2020-06-15 11:45:59 +0800

    微信小程序开发实战第九讲之轮播

--- pages/mainPage/mainPage.js
+++ pages/mainPage/mainPage.js
@@ -9,13 +9,25 @@ Page({
     data: {
         statusBarHeight: app.globalData.statusBarHeight,
         navBarHeight: app.globalData.navgationHeight,
+
+        indicatorDots: true,
+        autoplay: true,
+        interval: 2000,
+        duration: 500,
+        bannersList:[],
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        var list = this.data.bannersList
+        list.push({"id":"0","url":"https://hbimg.huabanimg.com/cf1ec32b9b7d36c69a50867065d6971c1ab2f5c2617090-HhRm8v_fw658"})
+        list.push({"id":"0","url":"https://hbimg.huabanimg.com/1a6ec38ef37df0a18cbfd1c3e8da8a22060980e8442bd-hnq4qL_fw658"})
+        list.push({"id":"0","url":"https://hbimg.huabanimg.com/f371ce205a370ee377a3395217cc42ce73e0f27211fd61-4OFJiD_fw658"})
+        list.push({"id":"0","url":"https://hbimg.huabanimg.com/4f44e407a57855a941df7d51b0cc0f96df60c473a9c7-eAXyH7_fw658"})
 
+        this.setData({bannersList:list})
     },
 
     /**
